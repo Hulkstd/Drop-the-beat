df0fd15bb5bc37d668b5eed354df0945a36edf70
HulkSTD <hulk1257@gmail.com>
2019-09-19 17:14:18 +0900

    ADD Combo & Judgement UI Animation

--- Assets/Scripts/UI/Animation/ComboText.cs
+++ Assets/Scripts/UI/Animation/ComboText.cs
@@ -1,7 +1,39 @@
-﻿namespace UI.Animation
+﻿using System;
+using UnityEngine;
+
+namespace UI.Animation
 {
     public class ComboText : TextChangedEvent<ComboText>
     {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _animationName;
+        private int _animationNameCache = int.MinValue;
         
+        public void GainCombo(int Value)
+        {
+            Text = (int.Parse(Text) + Value).ToString();
+        }
+
+        public void ResetCombo()
+        {
+            var tmp = _onTextChanged;
+            _onTextChanged.RemoveAllListeners();
+            Text = 0.ToString();
+            _onTextChanged = tmp;
+        }
+
+        public void PlayAnimation()
+        {
+            if (_animationNameCache == int.MinValue)
+            {
+                _animationNameCache = Animator.StringToHash(_animationName);
+            }
+            _animator.Play(_animationNameCache);
+        }
+
+        private void Awake()
+        {
+            Text = 0.ToString();
+        }
     }
 }
--- Assets/Scripts/UI/Animation/JudgementText.cs
+++ Assets/Scripts/UI/Animation/JudgementText.cs
@@ -1,7 +1,42 @@
-﻿namespace UI.Animation
+﻿using System;
+using UnityEngine;
+
+namespace UI.Animation
 {
     public class JudgementText : TextChangedEvent<JudgementText>
     {
+        [Serializable]
+        public enum Judgement
+        {
+            Excelent = 1,
+            Great = 2,
+            Good = 3,
+            Bad = 4,
+            Miss = 5,
+        }
         
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _animationName;
+        private int _animationNameCache = int.MinValue;
+
+        public void Judge(Judgement judge)
+        {
+            Text = judge.ToString();
+            _onTextChanged.Invoke();
+        }
+        public void Judge(int judge)
+        {
+            Text = ((Judgement)judge).ToString();
+            _onTextChanged.Invoke();
+        }
+        
+        public void PlayAnimation()
+        {
+            if (_animationNameCache == int.MinValue)
+            {
+                _animationNameCache = Animator.StringToHash(_animationName);
+            }
+            _animator.Play(_animationNameCache);
+        }
     }
 }
