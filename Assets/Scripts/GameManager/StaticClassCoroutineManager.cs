7360190f2834906090a428af609cde7b15f1b03f
HulkSTD <hulk1257@gmail.com>
2019-09-23 14:35:44 +0900

    Add StaticClassCoroutineManager.cs

---
+++ Assets/Scripts/GameManager/StaticClassCoroutineManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace GameManager
+{
+    public class StaticClassCoroutineManager : Singleton<StaticClassCoroutineManager>
+    {
+        public void Coroutine(IEnumerator coroutine)
+        {
+            StartCoroutine(coroutine);
+        }
+    }
+}
